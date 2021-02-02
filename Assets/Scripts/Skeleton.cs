bf2e687484d6df32e48627fb2c44acba97725c05
Ryan Chakov <ryanchakov@gmail.com>
2021-02-01 22:08:02 -0500

    Skeleton Fixes?
    It is basically the other script with some small changes

--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -4,36 +4,34 @@ using UnityEngine;
 
 public class Skeleton : MonoBehaviour
 {
+    public float offset = 0;
     public Animator skeleton;
     public TriggerSet ts;
-    public GameObject player;
+    public GameObject player, target;
     public SkinnedMeshRenderer skin;
     public GameObject root;
     public bool first = false;
-    public CharacterController me;
     Vector3 home;
+    RaycastHit hit = new RaycastHit();
+    public CharacterController enemy;
     // Start is called before the first frame update
     void Start()
     {
         home = transform.position;
-        print(home.ToString());
     }
 
     // Update is called once per frame
     void Update()
     {
-        root.transform.localPosition = new Vector3(0,root.transform.localPosition.y,0);
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            print(transform.position.ToString());
-        }
+        root.transform.localPosition = new Vector3(0, root.transform.localPosition.y, 0);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 10, Color.cyan);
         if (Vector3.Distance(transform.position, player.transform.position) < 30f && !first)
         {
             skin.enabled = true;
             skeleton.Play("comeOutOfTheGround2handed_B");
             first = true;
         }
-        if (ts.fetch && Vector3.Distance(transform.position,player.transform.position)>1.3f)
+        if (ts.fetch && Vector3.Distance(transform.position, player.transform.position) > 1.3f)
         {
             //Play the animation
             skeleton.Play("2handedRun");
@@ -44,18 +42,34 @@ public class Skeleton : MonoBehaviour
             transform.LookAt(lookAtPosition);
 
             //Move Torwards the Player
+           /* Vector3 forward = transform.TransformDirection(Vector3.forward);
+            float speedey = 2 * .5f*Time.deltaTime; 
+            enemy.SimpleMove(forward * speedey);
+            */
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 3 * Time.deltaTime);
 
-            // transform.position = Vector3.MoveTowards(transform.position,player.transform.position,2*Time.deltaTime);
-            Vector3 stop = transform.position-Vector3.MoveTowards(transform.position, player.transform.position, 2 * Time.deltaTime);
-            me.SimpleMove(stop);
+            /*
+              Vector3 lookAtPosition = player.transform.position;
+            lookAtPosition.y = transform.position.y;
+            transform.LookAt(lookAtPosition);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2f) && hit.transform.tag == "Enemy")
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 2 * Time.deltaTime);
+            }
+            else if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 15f) && hit.transform.tag == "Enemy")
+            {
+                print("I am standing ON someone");
+                transform.position = Vector3.MoveTowards(transform.position, home, 2 * Time.deltaTime);
+            } 
+            */
         }
         else if (Vector3.Distance(transform.position, player.transform.position) <= 1.3f)
         {
-            skeleton.Play("2handedAttack1");
+            skeleton.Play("2handedAttack1");         
         }
         else
         {
-            if (Vector3.Distance(transform.position,home) > .001f)
+            if (Vector3.Distance(transform.position, home) > .001f)
             {
                 transform.position = Vector3.MoveTowards(transform.position, home, 3 * Time.deltaTime);
                 Vector3 lookAtPosition = home;
@@ -65,9 +79,10 @@ public class Skeleton : MonoBehaviour
             }
             else if (!skeleton.GetCurrentAnimatorStateInfo(0).IsName("comeOutOfTheGround2handed_B") && first)
             {
-             skeleton.Play("idle2handed");
+                skeleton.Play("idle2handed");
             }
-           
+
         }
     }
+    
 }
